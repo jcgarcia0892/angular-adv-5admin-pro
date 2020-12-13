68bbc6e9f29d2f05bbfdb83ebf59c5b4efb96766
Julio Garcia <juliogarcia@MacBook-Air-de-Equipo.local>
2020-12-13 18:14:20 -0300

    Fin sección 19 despliegue de la app

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanLoad, Route, UrlSegment } from '@angular/router';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { UsuarioService } from '../services/usuario.service';
@@ -7,10 +7,20 @@ import { UsuarioService } from '../services/usuario.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(  private usuarioService: UsuarioService,
-                private router:Router) {}
+                private router: Router) {}
+  canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.usuarioService.validarToken()
+        .pipe(
+          tap((estaAutenticado: boolean) => {
+            if (!estaAutenticado) {
+              this.router.navigate(['/login']);
+            }
+          })
+        );
+  }
 
   canActivate(
     next: ActivatedRouteSnapshot,
@@ -18,13 +28,11 @@ export class AuthGuard implements CanActivate {
 
       return this.usuarioService.validarToken()
         .pipe(
-          tap((estaAutenticado:boolean) => {
-            if(!estaAutenticado) {
+          tap((estaAutenticado: boolean) => {
+            if (!estaAutenticado) {
               this.router.navigate(['/login']);
             }
           })
-        )
-      
+        );
   }
-  
 }
--- src/app/pages/pages.routing.module.ts
+++ src/app/pages/pages.routing.module.ts
@@ -1,20 +1,9 @@
-import { AdminGuard } from './../guards/admin.guard';
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
-import { AuthGuard } from './../guards/auth.guard';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { Grafica1Component } from './grafica1/grafica1.component';
-import { HospitalesComponent } from './mantenimiento/hospitales/hospitales.component';
-import { MedicoComponent } from './mantenimiento/medicos/medico.component';
-import { MedicosComponent } from './mantenimiento/medicos/medicos.component';
 import { NgModule } from '@angular/core';
-import { PagesComponent } from './pages.component';
-import { PerfilComponent } from './perfil/perfil.component';
-import { ProgressComponent } from './progress/progress.component';
-import { PromesasComponent } from './promesas/promesas.component';
 import { RouterModule, Routes } from '@angular/router';
-import { RxjsComponent } from './rxjs/rxjs.component';
-import { UsuariosComponent } from './mantenimiento/usuarios/usuarios.component';
-import { BusquedaComponent } from './busqueda/busqueda.component';
+import { AuthGuard } from './../guards/auth.guard';
+
+import { PagesComponent } from './pages.component';
+
 
 // Pages
 
@@ -25,25 +14,8 @@ const routes: Routes = [
         path: 'dashboard',
         component: PagesComponent,
         canActivate: [ AuthGuard ],
-        children: [
-          { path: '',  component: DashboardComponent, data: {titulo: 'Dashboard'} },
-          { path: 'account-settings',   component: AccountSettingsComponent, data: {titulo: 'Configuración de cuentas'} },
-          { path: 'buscar/:termino',   component: BusquedaComponent, data: {titulo: 'Busquedas'} },
-          { path: 'grafica1',   component: Grafica1Component, data: {titulo: 'Gráficas'} },
-          { path: 'perfil',   component: PerfilComponent, data: {titulo: 'Perfil'} },
-          { path: 'progress',   component: ProgressComponent, data: {titulo: 'ProgressBar'} },
-          { path: 'promesas',   component: PromesasComponent, data: {titulo: 'Promesas'} },
-          { path: 'rxjs',   component: RxjsComponent, data: {titulo: 'RxJs'} },
-
-          // Mantenimientos
-
-          { path: 'hospitales', component: HospitalesComponent, data: {titulo: 'Hospitales'} },
-          { path: 'medicos', component: MedicosComponent, data: {titulo: 'Medicos Component'} },
-          { path: 'medico/:id', component: MedicoComponent, data: {titulo: 'Medico'} },
-            // Solo Usuarios con un rol Admin pueden ver esta página
-          { path: 'usuarios', canActivate: [ AdminGuard ], component: UsuariosComponent, data: {titulo: 'Usuarios'} },
-
-        ]
+        canLoad: [AuthGuard],
+        loadChildren: () => import('./child-routes.module').then(m => m.ChildRoutesModule)
     },
 
 ];
